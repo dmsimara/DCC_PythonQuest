96ff4823b32f3162d5c44e4454d49daf383de843
dmsimara <dmsimara@iskolarngbayan.pup.edu.ph>
2023-12-22 22:54:40 -0800

    updated department_caption.py

---
+++ department_caption.py
@@ -0,0 +1,7 @@
+name = input("Indicate your name: ")
+department = input("Enter your AWS Cloud Club department: ")
+galaxy = input("Specify the galaxy to which you belong: ")
+
+caption = "\nYour Department's DP Blast Caption:\n\n🚀 Deploying...I'm %s, representing the %s. I'm thrilled to commence our expedition into the vast and uncharted realm of the %s. Together, we'll navigate this exciting frontier of techology! 🌌🌟💻" % (name, department, galaxy)
+
+print(caption)
