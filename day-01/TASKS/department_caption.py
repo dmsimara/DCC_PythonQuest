d484d56484b513fdbf7b632c38152374300d1daa
dmsimara <dmsimara@iskolarngbayan.pup.edu.ph>
2023-12-22 22:59:17 -0800

    updated task for department_caption.py

--- day-01/TASKS/department_caption.py
+++ day-01/TASKS/department_caption.py
@@ -1,15 +1,7 @@
-# Template for department's caption
-""" 🚀Deploying...I'm <𝗡𝗮𝗺𝗲>, representing the <𝗗𝗲𝗽𝗮𝗿𝘁𝗺𝗲𝗻𝘁>. I'm thrilled to commence our expedition into the vast and uncharted realm of the <𝗚𝗮𝗹𝗮𝘅𝘆>. Together, we'll navigate this exciting frontier of technology! 🌌🌟💻 """
+name = input("Indicate your name: ")
+department = input("Enter your AWS Cloud Club department: ")
+galaxy = input("Specify the galaxy to which you belong: ")
 
-# Write the code ↓ to read user's input.
+caption = "\nYour Department's DP Blast Caption:\n\n🚀 Deploying...I'm %s, representing the %s. I'm thrilled to commence our expedition into the vast and uncharted realm of the %s. Together, we'll navigate this exciting frontier of techology! 🌌🌟💻" % (name, department, galaxy)
 
-
-
-
-
-# Write the code ↓ to display the department's caption.
-# Select and employ a string concatenation method based on your personal preference and comfort level.
-
-
-
-
+print(caption)
