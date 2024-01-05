8ec3c68f212ea47b1053aabc44a1e312fa1dfd16
dmsimara <dmsimara@iskolarngbayan.pup.edu.ph>
2024-01-05 11:02:05 -0800

    updated task for perfect-number_determinator.py

--- day-03/TASKS/perfect-number_determinator.py
+++ day-03/TASKS/perfect-number_determinator.py
@@ -1,28 +1,19 @@
-# Write the code ↓ to read the user's input for a positive integer.
-# Be cautious when reading input of various data types.
+print("\nPERFECT NUMBER DETERMINATOR FOR ALF\n")
+number = int(input("Please, enter a non-negative number: "))
+orig_num = number
 
+sum_divisors = 0
+perfect = 1
 
+if number < 0:
+    print("Negative numbers can't be a perfect number.")
+else:
+    while perfect < number:
+        if number % perfect == 0:
+            sum_divisors += perfect
+        perfect += 1
 
-
-
-
-
-
-
-
-# Write the code ↓ to check if the entered integer is a Perfect Number using a loop.
-
-
-
-
-
-
-
-# Write the code ↓ to display the Perfect Number check result.
-# NOTE : You can use if-else statement or ternary operator to display the result.
-
-
-
-
-
-
+    if number == sum_divisors:
+        print("%d is a Perfect Number." % (orig_num))
+    else:
+        print("%d is NOT a Perfect Number." % (orig_num))
