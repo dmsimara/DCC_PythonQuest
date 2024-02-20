6ae80fabd741d2c0cd1a0d548235fe882414f8a1
dmsimara <dmsimara@iskolarngbayan.pup.edu.ph>
2024-02-19 16:21:34 -0800

    added requirements.txt + updated main.py

--- day-13/Submission/main.py
+++ day-13/Submission/main.py
@@ -1,15 +1,25 @@
+import csv
 from flask import Flask, jsonify  
 
 app = Flask(__name__)
 
 csv_file = 'programming_languages.csv'
 
-def read_csv():
-    pass
+def read_csv(csv_file):
+    
+    content = []
+    with open(csv_file, 'r') as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            content.append(row)
+
+    return (content)
 
 @app.route('/', methods=['GET'])
 def get_langs():
-    pass
+
+    content = read_csv(csv_file)
+    return jsonify(content)
 
 if __name__ == '__main__':
     app.run(debug=True)
